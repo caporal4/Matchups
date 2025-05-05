428e277ea426f855db5d7ca7209bc0bde15006ea
Brendan Caporale <bcaporale96@gmail.com>
2025-05-05 13:53:16 -0600

    Initial Commit

---
+++ Matchups/MatchupsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchupsApp.swift
+//  Matchups
+//
+//  Created by Brendan Caporale on 5/5/25.
+//
+
+import SwiftUI
+
+@main
+struct MatchupsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
